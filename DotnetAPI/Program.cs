071391370435096e79577d30910bf71a77b2c19a
Ahmed Rezk <rezk5024@gmail.com>
2024-07-18 18:10:00 -0600

    60. API Setup

--- DotnetAPI/Program.cs
+++ DotnetAPI/Program.cs
@@ -7,16 +7,46 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(
+    (corsBuilder) =>
+    {
+        corsBuilder.AddPolicy(
+            "DevCors",
+            (corsBuilder) =>
+            {
+                corsBuilder
+                    .WithOrigins("http://localhost:3000")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            }
+        );
+        corsBuilder.AddPolicy(
+            "ProdCors",
+            (corsBuilder) =>
+            {
+                corsBuilder
+                    .WithOrigins("http://myProductionSite.com")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            }
+        );
+    }
+);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors("DevCors");
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 else
 {
+    app.UseCors("ProdCors");
     app.UseHttpsRedirection();
 }
 
