1a69131e052edfab51f3ac0a9462f61ef9100fea
Ahmed Rezk <rezk5024@gmail.com>
2024-07-30 15:58:21 -0600

    user auth models

--- DotnetAPI/Models/User.cs
+++ DotnetAPI/Models/User.cs
@@ -18,4 +18,37 @@ namespace DotNet.Models
         public string Gender { get; set; } = "";
         public bool Active { get; set; }
     }
+
+    public partial class UserRegistrationDto
+    {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Gender { get; set; } = "";
+        public string Password { get; set; } = "";
+        public string PasswordConfirmation { get; set; } = "";
+    }
+
+    public partial class UserLoginDto
+    {
+        public string Email { get; set; } = "";
+        public string Password { get; set; } = "";
+    }
+
+    public partial class UserPasswordConfirmationDto
+    {
+        public string Password { get; set; } = "";
+        public string PasswordConfirmation { get; set; } = "";
+    }
+
+    public partial class UserAuth
+    {
+        public string Email { get; set; } = "";
+
+        public string PasswordHash { get; set; } = "";
+        public string PasswordSalt { get; set; } = "";
+
+        // public byte[] PasswordHash { get; set; } = new byte[0];
+        // public byte[] PasswordSalt { get; set; } = new byte[0];
+    }
 }
