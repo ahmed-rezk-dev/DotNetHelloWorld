e26d6fe5fe30c6b934c711381ab6e126cce27219
Ahmed Rezk <rezk5024@gmail.com>
2024-08-01 14:39:24 -0600

    posts model

---
+++ DotnetAPI/Models/Post.cs
@@ -0,0 +1,18 @@
+namespace DotNetAPI.Models
+{
+    public partial class Post
+    {
+        public int PostId { get; set; }
+        public int UserId { get; set; }
+        public string PostTitle { get; set; } = "";
+        public string PostContect { get; set; } = "";
+        public DateTime PostCreated { get; set; }
+        public DateTime PostUpdated { get; set; }
+    }
+
+    public partial class PostDto
+    {
+        public string PostTitle { get; set; } = "";
+        public string PostContect { get; set; } = "";
+    }
+}
